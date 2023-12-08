2f536035cc76760279743dea35b9f8736a286ecc
DevUreak <rlaquddlf3204@gmail.com>
2023-12-08 19:32:56 +0900

    test : unit test init update

--- test/unit/Game.ts
+++ test/unit/Game.ts
@@ -1,20 +1,21 @@
 import { expect } from "chai";
 import { ethers } from "@coinmeca/ethers";
-// import { a } from "@coinmeca/ethers/utils";
-// import { PANIC_CODES } from '@nomicfoundation/hardhat-chai-matchers/panic';
-// import { Initialize } from "../../scripts/modules/deploy";
-// import { Order } from "@coinmeca/ethers/types";
-// import { AccountLike } from "@coinmeca/ethers/accounts";
-// import { FacetCutAction } from "@coinmeca/ethers/diamond";
-// import { get, getSelectors } from "../upgrade/utils";
+import { Contracts } from "../../scripts/Contracts";
 
 export default describe('# Unit Test : Game', () => {
 
     it('Initialize', async () => {
-    
-        describe('', () => {
+        const init = await Contracts();
+        const { Game } = init;
 
+        describe('', () => {
             it('test_facet : value check', async () => {
+                console.log(Game.address)
+                
+                await Game.test_set(12222);
+                expect(await Game.test_get()).to.be.equal(12222)
+                
+                
         
             
 
