0ea33c5388ebc40f72b7fb9b6772b63c886d53da
DevUreak <rlaquddlf3204@gmail.com>
2023-12-10 17:24:00 +0900

    feat : type update

--- contracts/types/Errors.sol
+++ contracts/types/Errors.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.22;
 
-// import {Type} from "modules/history/types/Type.sol";
-
 interface Errors {
     error NO_PERMISSION(address);
+    error NOT_POSSIBLE();
 }
--- contracts/types/Type.sol
+++ contracts/types/Type.sol
@@ -4,5 +4,16 @@ pragma solidity ^0.8.22;
 // import {Type} from "modules/history/types/Type.sol";
 
 interface Type {
+    enum State {
+        START,
+        END
+    }
 
+    struct WorldEvent {
+        uint index; // 식별 번호
+        uint amount; // 뿌릴양
+        uint start; // 시작 시간
+        uint duration; // 이벤트 기간
+        State state; // 이벤트 상태
+    }
 }
